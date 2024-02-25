0b9902438c88b1967c1a9e5950b2d4f71197a5ac
AlexFilonenko88 <af88_88@mail.ru>
2024-02-25 14:28:46 +0300

    delete file

--- local/templates/furniture_blue/footer.php
+++ local/templates/furniture_blue/footer.php
@@ -1,6 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-IncludeTemplateLangFile(__FILE__);
-?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 			</div>
 		</div>
 		<div id="space-for-footer"></div>
